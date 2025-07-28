55e94abb013593a8fb0f799c089824db0dfebc4c
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-07-28 14:48:08 +0530

    machine coding 'filter'

--- Assignments/machine-coding/index.js
+++ Assignments/machine-coding/index.js
@@ -1,10 +1,11 @@
+//--------------------- Map function ------------------------------- 
 const numbers = [1, 2, 3];
 const results = numbers.map((n) => n * 2);
 
 console.log({ results });
-//----------------------------------------------------
-if (!Array.prototype.My_Map) {
-    Array.prototype.My_Map = function(user_funciton) {
+//-------------------------------------------------------------------
+if (!Array.prototype.I_Map) {
+    Array.prototype.I_Map = function(user_funciton) {
         const result = [];
 
         for (let i = 0; i <this.length; i++){
@@ -16,6 +17,31 @@ if (!Array.prototype.My_Map) {
 }
 
 const arr = [1, 2, 3, 4, 5, 6];
-const n2 = arr.My_Map((e) => e * 3);
+const n2 = arr.I_Map((e) => e * 3);
 console.log(n2);
-//------------------------------------------------------
+
+//------------------ Filter funciton  ---------------------------
+
+const n =  [1, 2, 3, 4, 5, 6]
+const result = n.filter(( n ) => n % 2 == 0)
+
+console.log(result);
+//---------------------------------------------------------------
+if(!Array.prototype.I_Filter){
+    Array.prototype.I_Filter = function(user_funciton){
+        const result = [];
+
+        for (let i = 0; i < this.length; i++){
+            if(user_funciton(this[i])){
+                result.push(this[i])
+            }
+        }
+        return result;
+    }
+}
+
+const num = [1, 2, 3, 4, 5, 6, 6, 8, 9, 10]
+const n1 = num.I_Filter((n) => n % 2 == 0)
+console.log(n1);
+//---------------------------------------------------
+
