59e0f6476360f8bdb203ef5dc71635237f8eeeea
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-08-11 10:12:10 +0530

    Questions: prototype 'animal some genric sound question'

---
+++ Questions/prototype.js
@@ -0,0 +1,10 @@
+//1 
+function Animal(name) {
+    // Initialize name property
+    this.name = name
+}
+
+Animal.prototype.makeSound = function(){
+  return "Some generic sound";
+}
+//-----------------------------------------------
